a2e70b9d477f1c3202c2491f964b61348e3a089f
Jonathan Greenemeier <jon@6eDesign.net>
2018-09-24 21:43:55 -0600

    Init package

---
+++ dist/simplebind-redux-devtools.esm.js
@@ -0,0 +1,42 @@
+import simpleBind from 'simplebind.js';
+
+let { bind, registerPlugin, util } = simpleBind;
+let state = simpleBind.getState();
+
+const useDevTools = typeof window.__REDUX_DEVTOOLS_EXTENSION__ != 'undefined'; 
+let devTools, isBindDueToDevTools = false;
+if(useDevTools) { 
+  devTools = window.__REDUX_DEVTOOLS_EXTENSION__.connect({latency: 0});
+  devTools.subscribe((message,...args) => {
+    if (message.type === 'DISPATCH' && message.state) {
+      let newState = JSON.parse(message.state); 
+      for(var key in newState) {
+        isBindDueToDevTools = true;
+        bind(key,newState[key]);
+      }
+    }
+  });
+}
+
+let isARepeatKey = name => name.indexOf('__repeat') == 0;
+
+let removeRepeatsFromState = obj => { 
+  let newObj = util.extend({},state.boundObjects); 
+  Object.keys(newObj).filter(isARepeatKey).forEach(k => delete newObj[k]);
+  return newObj;
+}; 
+
+let onBind = (objName,obj) => { 
+  if(isARepeatKey(objName)) return; 
+  var eligibleForDevTools = isBindDueToDevTools == false; 
+  if(isBindDueToDevTools) isBindDueToDevTools = false;
+  if(!isARepeatKey(objName) && eligibleForDevTools) {
+    devTools.send(`${objName}-bound`, removeRepeatsFromState(state.boundObjects));
+  } 
+  return obj;
+};
+
+registerPlugin({
+  name: 'reduxDevToolsConnector',
+  postBind: onBind
+});
---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from "rollup-plugin-uglify";
+import buble from 'rollup-plugin-buble';
+import pkg from './package.json';
+
+let isProd = process.env.buildTarget == 'prod';
+
+let plugins = [
+	resolve(),
+	commonjs(), 
+	buble()
+]; 
+
+if(isProd) { 
+	plugins.push(uglify());
+}
+
+export default [
+	// browser-friendly UMD build
+	{
+    input: 'index.js',
+    external: ['simplebind.js'],
+		output: {
+			name: 'simplebind-redux-devtools',
+			file: pkg.browser,
+			format: 'umd',
+			sourcemap: true
+		},
+		plugins
+	},
+
+	// CommonJS (for Node) and ES module (for bundlers) build.
+	// (We could have three entries in the configuration array
+	// instead of two, but it's quicker to generate multiple
+	// builds from a single configuration where possible, using
+	// an array for the `output` option, where we can specify 
+	// `file` and `format` for each target)
+	{
+		input: 'index.js',
+		external: ['simplebind.js'],
+		output: [
+			{ file: pkg.main, format: 'cjs' },
+			{ file: pkg.module, format: 'es' }
+		]
+	}
+];
